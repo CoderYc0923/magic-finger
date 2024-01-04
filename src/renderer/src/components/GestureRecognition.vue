cf174d96d2c2bdc5edc62c5f1c1aa2c92cc9d212
yechao <yechao@sucsoft.com>
2024-01-04 15:36:25 +0800

    feat: 视频流工程编写中

---
+++ src/renderer/src/components/GestureRecognition.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <video id="webcam" autoplay playsinline width="640" height="480"></video>
+    <canvas id="canvas" class="d-none"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+//@ts-ignore
+import Webcam from 'webcam-easy';
+
+</script>
+
+<style></style>
